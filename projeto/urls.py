a4c171a99aad18126997df3dabf06f9ffa1f0003
vitor <vitor.scastro2013@hotmail.com>
2022-12-27 20:31:17 -0300

    Criando app

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,7 +15,25 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+# HTTP REQUEST <- HTTP RESPONSE
+
+#HTTP Request
+def home(request):
+    return HttpResponse('HOME')
+
+    # return HTTP Response
+
+def sobre(request):
+    return HttpResponse('SOBRE')
+
+def contato(request):
+    return HttpResponse('CONTATO')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home), # HOME
+    path('sobre/', sobre), # /sobre/
+    path('contato/', contato), # /contato/
 ]
